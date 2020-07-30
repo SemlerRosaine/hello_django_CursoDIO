d00c068cc81d174e823d90d35a9b4185d2a1da8f
Rosaine Fiorio Semler <67932577+SemlerRosaine@users.noreply.github.com>
2020-07-29 21:03:18 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos<h1>'.format(nome, idade))
+def soma(request, valor_a, valor_b):
+    return HttpResponse('<h1> Calculadora: \n</h1><h2>Soma {} + {} = {}</h2>'.format(valor_b, valor_b, (valor_a+valor_b)))
+
+def subtracao(request, valor_a, valor_b):
+    return HttpResponse('<h1> Calculadora: \n</h1><h2>Subtração {} - {} = {}</h2>'.format(valor_b, valor_b, (valor_a-valor_b)))
+
+def divisao(request, valor_a, valor_b):
+    return HttpResponse('<h1> Calculadora: \n</h1><h2>Divisão {} / {} = {}</h2>'.format(valor_b, valor_b, (valor_a/valor_b)))
+
+def multiplicacao(request, valor_a, valor_b):
+    return HttpResponse('<h1> Calculadora: \n</h1><h2>Multiplicação {} * {} = {}</h2>'.format(valor_b, valor_b, (valor_a*valor_b)))
